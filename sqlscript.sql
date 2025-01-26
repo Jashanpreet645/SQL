0de9fd7995b250bbc1de9cc879a8bb8a8c9df4b9
Jashanpreet Singh <officialjashan645@gmail.com>
2025-01-26 19:58:38 +0530

    Add files via upload

---
+++ sqlscript.sql
@@ -0,0 +1,54 @@
+REM   Script: LAB assignment-1(part-1)
+REM   SQL Basics
+
+create table student(  
+    Rno int,  
+    Name varchar(50),  
+    DOB varchar(50),  
+    Gender varchar(50),  
+    Class int,  
+    Collage varchar(50),  
+    City varchar(50),  
+    Marks int  
+);
+
+create table student(  
+    Rno int,  
+    Name varchar(50),  
+    DOB varchar(50),  
+    Gender varchar(50),  
+    Class int,  
+    Collage varchar(50),  
+    City varchar(50),  
+    Marks int  
+) 
+ 
+insert into student values(1023,'marko','12-mar-2005','male',12,'thapar','patiala',0);
+
+insert into student values(102317005,'marko','12-mar-2005','male',12,'thapar','patiala',0);
+
+insert into student values(102317003,'abc','1-mar-2005','male',12,'thapar','amritsar',60);
+
+insert into student values(102317002,'def','2-mar-2005','male',12,'thapar','ludhiana',560);
+
+insert into student(Rno,Name,DOB,Gender,Class,Collage,City,Marks) values(102317001,'jkl','10-mar-2005','male',12,'thapar','unknown',03);
+
+select * from student;
+
+insert into student values(102317000,'ghi','17-mar-2005','male',12,'thapar','patiala',30);
+
+select * from student;
+
+desc student
+
+
+select * from student where City = 'patiala';
+
+select Rno from student where City = 'patiala';
+
+select Rno and Name and Class from student where City = 'patiala';
+
+select Rno & Name & Class from student where City = 'patiala';
+
+select Rno , Name , Class from student where City = 'patiala';
+
